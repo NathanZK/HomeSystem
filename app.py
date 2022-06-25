6fe67da2a79c13075e93769576a7cb5d039a74ab
NathanZK <nathanzelalem100@gmail.com>
2022-06-26 02:39:52 +0300

    database fix

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from website import create_app
+from website import db, create_app
 
 
 app = create_app()
+db.create_all(app=create_app())
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=5000, debug=False)
--- website/__init__.py
+++ website/__init__.py
@@ -25,21 +25,14 @@ def create_app():
 
     from .models import User, Room
 
-    create_database(app)
+    # create_database(app)
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
   
     
-    db.create_all()
-
-    # with app.app_context():
-    #     db.create_all()
-
-
-
-
+    
 
     @login_manager.user_loader
     def load_user(id):
