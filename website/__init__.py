70f306275422f8d510dd9e7a51ad634ec001336d
NathanZK <nathanzelalem100@gmail.com>
2022-06-25 23:45:18 +0300

    Tweaked app

--- website/__init__.py
+++ website/__init__.py
@@ -10,7 +10,8 @@ DB_NAME = "smarthome.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'KEY'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+   # app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://kfsegtleleruby:39d083095d563f0d75063b66763547a92e4eafb172c17048af16f8e8dce8ba59@ec2-54-204-56-171.compute-1.amazonaws.com:5432/ddsab05614a43e'
     db.init_app(app)
 
     from .views import views
