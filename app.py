67f2009325e96a693b519e38d15d854e591ed37d
NathanZK <nathanzelalem100@gmail.com>
2022-06-26 02:54:06 +0300

    fix

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from website import db, create_app
 
-
 app = create_app()
-db.create_all(app=create_app())
 
+#db.create_all(app=create_app())
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=5000, debug=False)
    #app.run(debug=False)
